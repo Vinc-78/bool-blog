16330cb5ed0a2652c1b6e0f0e67dda1c8cd0acf6
Obez99 <giuliogx@live.it>
2022-01-14 17:35:59 +0100

    Fixes route after store function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -51,7 +51,7 @@ class PostController extends Controller
 
         $post->save();
 
-        return redirect()->route('home.index');
+        return redirect()->route('posts.index');
     }
 
     /**
